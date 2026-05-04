73023c4c6078321f78de57ba8ca3c4fbdbdb653b
FrankieJ13 <FrankieJ13@users.noreply.github.com>
2026-05-04 11:07:38 +0500

    Use online presence icons

--- js/app.js
+++ js/app.js
@@ -177,6 +177,7 @@ function syncTheme() {
     'dock-btn-grafik':      COSMIC_ICON_BASE + 'cosmic_grafik.svg',
     'dock-btn-instruktsii': COSMIC_ICON_BASE + 'cosmic_faq.svg',
     'dock-btn-vizity':      COSMIC_ICON_BASE + 'cosmic_vizity.svg',
+    'btn-presence':         COSMIC_ICON_BASE + 'cosmic_online.svg',
     'btn-refresh':          COSMIC_ICON_BASE + 'cosmic_refresh.svg',
     'btn-hamburger':        COSMIC_ICON_BASE + 'cosmic_menu.svg',
     // Гамбургер
@@ -193,6 +194,7 @@ function syncTheme() {
     'dock-btn-grafik':      DEFAULT_ICON_BASE + 'grafik.svg',
     'dock-btn-instruktsii': DEFAULT_ICON_BASE + 'faq.svg',
     'dock-btn-vizity':      DEFAULT_ICON_BASE + 'vizity.svg',
+    'btn-presence':         DEFAULT_ICON_BASE + 'online.svg',
     'btn-refresh':          DEFAULT_ICON_BASE + 'refresh.svg',
     'btn-hamburger':        DEFAULT_ICON_BASE + 'menu.svg',
     'hmb-month-trigger':    DEFAULT_ICON_BASE + 'base.svg',

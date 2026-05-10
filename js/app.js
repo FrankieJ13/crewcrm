e365dfd08f33490e90b094cef262ad93105ab050
FrankieJ13 <frankie13.jung@gmail.com>
2026-05-10 12:53:03 +0500

    Inline SVG icons in bulk editor buttons to fix inconsistent loading

--- js/app.js
+++ js/app.js
@@ -3465,10 +3465,10 @@ function openScheduleBulkEditor() {
         <span class="sched-bulk-status" id="sched-bulk-status"></span>
         <div class="sched-bulk-actions">
           <button class="sched-bulk-icon-btn" onclick="closeScheduleBulkEditor()" title="Выход из редактирования">
-            <span class="sched-bulk-icon" style="--sched-bulk-icon:url('${DEFAULT_ICON_BASE}grafik-cancel.svg')"></span>
+            <svg class="sched-bulk-svg" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16" fill="currentColor"><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5m9.954 3H2.545c-.3 0-.545.224-.545.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5m-6.6 5.146a.5.5 0 1 0-.708.708L7.293 10l-1.147 1.146a.5.5 0 0 0 .708.708L8 10.707l1.146 1.147a.5.5 0 0 0 .708-.708L8.707 10l1.147-1.146a.5.5 0 0 0-.708-.708L8 9.293z"/></svg>
           </button>
           <button class="sched-bulk-icon-btn" onclick="saveScheduleBulkEditor()" title="Сохранить график">
-            <span class="sched-bulk-icon" style="--sched-bulk-icon:url('${DEFAULT_ICON_BASE}grafik-save.svg')"></span>
+            <svg class="sched-bulk-svg" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 16 16" fill="currentColor"><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5m9.954 3H2.545c-.3 0-.545.224-.545.5v1c0 .276.244.5.545.5h10.91c.3 0 .545-.224.545-.5v-1c0-.276-.244-.5-.546-.5m-2.6 5.854a.5.5 0 0 0-.708-.708L7.5 10.793L6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0z"/></svg>
           </button>
         </div>
       </div>
